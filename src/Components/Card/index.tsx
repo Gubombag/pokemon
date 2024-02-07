6a2bc8b9ceb47f86b73205a9b511679ec68f58f5
Gustavo <gustavomoraessss93@hotmail.com>
2024-02-07 19:07:37 -0300

    .

---
+++ src/Components/Card/index.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {Container, Li, Ul} from "./styles"
+import axios from "axios";
+import { url } from "inspector";
+
+type DataPokemonProps = {
+    api: string;
+}
+
+const Card = () => {
+    
+      const [data, setData] = React.useState()
+      const [loading, setLoading] = React.useState<boolean>()
+      
+      const fetchData = () => {
+        setLoading(true)
+        axios
+        .get("https://pokeapi.co/api/v2/pokemon/zapdos")
+        .then((response) => {
+        setLoading(false)
+        setData(response.data)
+        })
+        .catch((error) => {
+        console.log(error);
+        })
+      }
+    
+React.useEffect(() => {
+    fetchData()
+}), []
+
+    return(
+        <>
+            <Container>
+                <Ul>
+                    {loading ? (
+                        <h1>Loading...</h1>
+                    ) : (
+                        <Li>{JSON.stringify(data)}</Li>
+                    )}
+                </Ul>
+            </Container>
+        </>
+    )
+}
+
+export default Card;
---
+++ src/Components/Card/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    background-color: #fff;
+    width: 100%;
+    height: 100vh;
+`
+
+export const Ul = styled.ul`
+
+`
+
+export const Li = styled.li`
+
+`
