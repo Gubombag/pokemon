255abe517915c86bd5080fee09973ba68d5cd79d
Gustavo <gustavomoraessss93@hotmail.com>
2024-02-08 20:16:57 -0300

    .

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -5,24 +5,17 @@ import axios from "axios";
 const Card = () => {
     
       const [data, setData] = React.useState()
-      const [loading, setLoading] = React.useState<boolean>()
       
       const fetchData = () => {
-        setLoading(true)
         axios
         .get("https://pokeapi.co/api/v2/pokemon/zapdos")
         .then((response) => {
         setData(response.data)
-        setLoading(false)
         })
         .catch((error) => {
         console.log(error);
         })
       }
-
-const handle = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    e.preventDefault()
-}
     
 React.useEffect(() => {
     fetchData()
@@ -32,11 +25,7 @@ React.useEffect(() => {
         <>
             <Container>
                 <Ul>
-                    {loading ? (
-                        <h1>Loading...</h1>
-                    ) : (
-                        <Li>{JSON.stringify(data)}</Li>
-                    )}
+                    <Li>{JSON.stringify(data)}</Li>
                 </Ul>
             </Container>
         </>
