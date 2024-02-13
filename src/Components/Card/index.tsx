4eea976961ea5b554e1372a0581dc841cb5f2849
Artur Moraes <arturmfilho@gmail.com>
2024-02-13 19:30:37 -0300

    fix metadata, styles, request, useState's and react components

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -1,60 +1,50 @@
 import React from "react";
-import {Container, Li, Ul} from "./styles"
+import { Container, Title } from "./styles";
 import axios from "axios";
+import Image from "next/image";
 
 const Card = () => {
-    
-    const [dataSprites, setDataSprites] = React.useState<any>()
-    const [dataSpecies, setDataSpecies] = React.useState<any>()
-      
-    const fetchData = () => {
-        axios
-        .get("https://pokeapi.co/api/v2/pokemon/zapdos")
-        .then((response) => {
-        const sprites = []
-        sprites.push(response.data.sprites.back_default)
-        sprites.push(response.data.sprites.back_shiny)
-        sprites.push(response.data.sprites.front_default)
-        sprites.push(response.data.sprites.front_shiny)
-        setDataSprites(sprites)
-        })
-        .catch((error) => {
+  const [dataSprites, setDataSprites] = React.useState<string[]>([]);
+  const [name, setName] = React.useState<string>("");
+  const [loading, setLoading] = React.useState<boolean>(true);
+
+  const fetchData = () => {
+    setLoading(true);
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/zapdos")
+      .then((response) => {
+        setDataSprites([
+          response.data.sprites.back_default,
+          response.data.sprites.back_shiny,
+          response.data.sprites.front_default,
+          response.data.sprites.front_shiny,
+        ]);
+        setName(response.data.species.name);
+      })
+      .catch((error) => {
         console.error(error);
-        })
-    }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
 
-    const fetchData1 = () => {
-        axios
-        .get("https://pokeapi.co/api/v2/pokemon/zapdos")
-        .then((response) => {
-            const species = [];
-            species.push(response.data.species.name);
-            species.push(response.data.species.url);
-            setDataSpecies(species);
-        })
-        .catch((error1) => {
-            console.error(error1);
-        })
-    }
-    
-React.useEffect(() => {
-    fetchData()
-}), []
+  React.useEffect(() => {
+    fetchData();
+  }, []);
 
-React.useEffect(() => {
-    fetchData1()
-}), []
-
-    return(
-        <>
-            <Container>
-                <Ul>
-                    <Li>{dataSprites}</Li><br></br>
-                    <Li>{dataSpecies}</Li>
-                </Ul>
-            </Container>
-        </>
-    )
-}
+  return loading ? (
+    <div>loading...</div>
+  ) : (
+    <Container>
+      <Title>{name}</Title>
+      <div>
+        {dataSprites?.map((s: string) => (
+          <Image key={s} alt={s} src={s} width={60} height={60} />
+        ))}
+      </div>
+    </Container>
+  );
+};
 
 export default Card;
--- src/Components/Card/styles.ts
+++ src/Components/Card/styles.ts
@@ -1,16 +1,16 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    display: flex;
-    background-color: #fff;
-    width: 100%;
-    height: 100vh;
-`
+  display: flex;
+  background-color: #fff;
+  width: 100%;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
 
-export const Ul = styled.ul`
-
-`
-
-export const Li = styled.li`
-
-`
+export const Title = styled.div`
+  font-size: 50px;
+  text-transform: capitalize;
+`;
