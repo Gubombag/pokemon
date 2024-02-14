7ef26df2eaba80c7da282c5d70d15d46dbf69b28
Artur Moraes <arturmfilho@gmail.com>
2024-02-13 21:39:40 -0300

    feat: pokemon's name input

--- src/Components/Card/styles.ts
+++ src/Components/Card/styles.ts
@@ -1,13 +1,29 @@
 import styled from "styled-components";
 
+export const ContainerForm = styled.div`
+  display: flex;
+  background-color: #fff;
+  width: 100%;
+  height: 40px;
+  justify-content: center;
+  align-items: center;
+  margin-top: 15px;
+  flex-direction: column;
+  text-align: center;
+`;
+
 export const Container = styled.div`
   display: flex;
   background-color: #fff;
   width: 100%;
-  height: 100vh;
+  height: calc(100vh - 55px);
   justify-content: center;
   align-items: center;
   flex-direction: column;
+`;
+
+export const LabelInput = styled.div`
+  margin-bottom: 5px;
 `;
 
 export const Title = styled.div`
